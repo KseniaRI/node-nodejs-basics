e47b884078d22793a7ea8741e3ccbb6a6db94cad
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-01-26 23:26:07 +0100

    feat: add create and copy functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import { mkdir, access, readdir, readFile, writeFile } from 'fs';
+import { resolve } from 'path';
+
+const destPath = resolve('src/fs/files_copy');
+const srcPath = resolve('src/fs/files');
+
 const copy = async () => {
-    // Write your code here 
+    access(destPath, (err) => {
+        if (!err) throw new Error("FS operation failed");
+        
+        mkdir(destPath, (err) => {
+            if (err) throw err;
+        }); 
+        readdir(srcPath, (err, files) => {
+            if (err) throw new Error("FS operation failed");;
+
+            files.map(file => {
+                readFile(`${srcPath}/${file}`, (err, data) => {
+                    if (err) throw err;
+                     
+                    writeFile(`${destPath}/${file}`, data, (err) => {
+                        if (err) throw err;
+                    }); 
+                }); 
+            })
+        })
+    })
 };
 
 await copy();
