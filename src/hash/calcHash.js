73ff008d6f855bd1a829c871be78bbb5b5859003
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-01-27 12:37:36 +0100

    feat: calc hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
+
+const filePath = resolve('src/hash/files/fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const input = createReadStream(filePath);
+
+    input.on('readable', () => {
+        const data = input.read();
+        if (data)
+           hash.update(data);
+        else {
+            console.log(hash.digest('hex'));
+        }
+    });
 };
 
 await calculateHash();
