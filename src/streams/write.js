335043dcd69c4aeb134476c6eb6a951b517bd429
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-01-29 10:59:50 +0100

    feat: zip

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from 'fs';
+import { resolve } from 'path';
+
+const filePath = resolve('src/streams/files/fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const fileStream = createWriteStream(filePath);
+    process.stdin.on('data', (chunk) => {
+        fileStream.write(chunk);
+        process.exit();
+    });
 };
 
 await write();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { resolve } from 'path';
+
+const zipPath = resolve('src/zip/files/archive.gz');
+const filePath = resolve('src/zip/files/fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    const gunzip = createGunzip();
+    const source = createReadStream(zipPath);
+    const destination = createWriteStream(filePath);
+
+    pipeline(source, gunzip, destination, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+          process.exitCode = 1;
+        } else {
+          console.log('File was decompressed');
+        }
+    });
 };
 
 await decompress();
